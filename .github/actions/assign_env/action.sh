e0ea23801f2c7d587e12b5bfa6e678072027d88a
Mohammed Abbas Kamaldeen <MohammedAbbas_Kamaldeen@Trimble.com>
2024-02-05 17:14:10 +0530

    updated the composite

--- .github/actions/assign_env/action.sh
+++ .github/actions/assign_env/action.sh
@@ -1,1 +1,8 @@
-echo "echo Goodbye" 
+
+if echo "${{ github.event.inputs.environment_name }}" | grep -Eq '^dev[1-9]$'; then
+    echo "env=dev"
+    github_env="dev"
+else
+    echo "env=${{ github.event.inputs.environment_name }}"
+    github_env=${{ github.event.inputs.environment_name }}
+fi
