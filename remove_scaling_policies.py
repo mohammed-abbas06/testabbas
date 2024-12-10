987c5c9696212f661ec1acb14af7364af6e175b6
mohammed-abbas06 <98637892+mohammed-abbas06@users.noreply.github.com>
2024-12-10 14:23:47 +0530

    Update remove_scaling_policies.py

--- remove_scaling_policies.py
+++ remove_scaling_policies.py
@@ -3,8 +3,8 @@ import boto3
 
 
 # Read environment variables and convert them into lists
-application_name = os.getenv('APPLICATION_NAME').split(',')
-deployment_group_name = os.getenv('DEPLOYMENT_GROUP_NAME').split(',')
+application_name = os.getenv('APPLICATION_NAME')
+deployment_group_name = os.getenv('DEPLOYMENT_GROUP_NAME')
 
 print(application_name)
 print(deployment_group_name)
